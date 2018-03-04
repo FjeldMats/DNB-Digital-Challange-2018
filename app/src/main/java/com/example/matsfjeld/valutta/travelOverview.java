6e80736ca1c82a3e63129d81f0764442251b184c
Kim <Kimrikters@gmail.com>
2018-03-04 13:22:57 +0100

    travel shortcut

--- app/src/main/java/com/example/matsfjeld/valutta/travelOverview.java
+++ app/src/main/java/com/example/matsfjeld/valutta/travelOverview.java
@@ -2,6 +2,7 @@ package com.example.matsfjeld.valutta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class travelOverview extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class travelOverview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_overview);
+        Bundle pac = getIntent().getExtras();
+        TextView am = findViewById(R.id.balance);
+        am.setText("You account balance:\n\n\n\n"+ pac.getDouble("amount"));
     }
 }
